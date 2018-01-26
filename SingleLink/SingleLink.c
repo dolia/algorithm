09b09e1c1c4e1eb9e06dcfb00aa2ad6ebb5c5b53
李东 <lidong6@xiaomi.com>
2018-01-26 17:04:05 +0800

    gcd

---
+++ SingleLink/SingleLink.c
@@ -0,0 +1,27 @@
+//
+//  BubbleSort.c
+//  algorithm
+//
+//  Created by dolia on 2018/1/25.
+//  Copyright © 2018年 dolia. All rights reserved.
+//
+
+#include <stdio.h>
+
+struct Node
+{
+	int data;  //数据区域
+	struct Node *next;	 //后驱指针
+};
+
+typedef struct Node singleList;
+
+char name ;
+
+
+int main (){
+
+	printf("Node\n");
+
+	return 0;
+};
