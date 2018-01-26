7bd00416d9b3b6e9370f6c2df18c524b33f8127e
李东 <lidong6@xiaomi.com>
2018-01-26 17:08:38 +0800

    fix

--- Gcd/gcd.c
+++ Gcd/gcd.c
@@ -5,40 +5,22 @@ int gcd(int x, int y){
 
 	int max,min,mod;
 
-	if (x>y){
-		max = x;
-		min = y;
-	}else{
-
-		max =y;
-		min = x;
-	}
+	max = x>y?x:y;
+	min = x<y?x:y;	
 
 	mod = max%min;
-
 	if (mod==0){
-
 		return min;
-
 	}else{
-
 		return gcd(max,mod);
 	}
-
-	return 0;
 }
 
 
 int  main(){
 
 	int x=1204,y=256,g=0;
-
-
 	g = gcd(x,y);
-
-
 	printf(" Greatest common divisor of %i and %i is : %i\n",x,y,g);
-
-
 	return 0;
 }
